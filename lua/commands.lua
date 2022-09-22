273892749b15f9f4007cd2d5115258134dd4802c
Kim Ravn Hansen <krh@cego.dk>
2022-09-22 15:08:28 +0200

    Only show cursorline in insert mode
    Also add DumpVim command

--- lua/commands.lua
+++ lua/commands.lua
@@ -5,20 +5,4 @@ end
 local api = vim.api
 
 api.nvim_create_user_command("Trim", [[%s/\s\+$//e]], {})
-
--------------------------
--- Cursor Line
--------------------------
---[[ 
-local cursorGroup = api.nvim_create_augroup("CursorLine", { clear = true })
-
-api.nvim_create_autocmd(
-  { "InsertLeave", "WinEnter" },
-  { pattern = "*", command = "set cursorline", group = cursorGroup }
-)
-api.nvim_create_autocmd(
-  { "InsertEnter", "WinLeave" },
-  { pattern = "*", command = "set nocursorline", group = cursorGroup }
-)
-]]
-
+api.nvim_create_user_command("DumpVim", function() dump(vim) end, {})

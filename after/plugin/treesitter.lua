d13f86ce8ad378172ed59fb75c0df27b4a25f800
Kim Ravn Hansen <krh@cego.dk>
2022-09-25 17:45:08 +0200

    Add help doc for treesitter.

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,3 +1,5 @@
+-- For windows, check this guide: https://github.com/nvim-treesitter/nvim-treesitter/wiki/Windows-support
+-- LLVM (clang) via chocolatey seems to work great.
 local status, ts = pcall(require, "nvim-treesitter.configs")
 if not status then
   print "Damn!. Could not load treesitter"
